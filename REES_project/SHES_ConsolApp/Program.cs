99eb42f27fd6aae9812c088c010725d4ae321a00
unknown <acoforgit@gmail.com>
2018-05-17 13:52:13 +0200

    Pokrenuo sam dva procesa (dva projekta - Battery, SolarPanel)

--- REES_project/SHES_ConsolApp/Program.cs
+++ REES_project/SHES_ConsolApp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,10 +9,25 @@ namespace SHES_ConsolApp
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
-
+            StartProcesses();
             Console.ReadKey();
+        }
+
+
+        public static void StartProcesses()
+        {
+            // Pokrecemo Battery projekat
+            Process p1 = new Process();
+            p1.StartInfo.FileName = @"..\..\..\Battery\bin\Debug\Battery.exe";
+            p1.Start();
+            
+            //Pokrecemo SolarPanel projekat
+            Process p2 = new Process();
+            p2.StartInfo.FileName = @"..\..\..\SolarPanel\bin\Debug\SolarPanel.exe";
+            p2.Start();
         }
     }
 }
